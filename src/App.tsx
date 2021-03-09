3a61cf8cd11ee9b53b4bfdd343eaa876f6445b5c
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-03-09 19:10:51 +0200

    [Feat] Home page structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,50 +2,60 @@ import React, { useState } from 'react';
 import Select from 'react-select';
 
 import Nav from './components/Nav';
+import { categories, difficultyLevel, questionType } from './constants';
 import './app.scss';
 
 const App = () => {
   const [page, setPage] = useState<string>('home');
   const [number, setNumber] = useState(null);
-  const [category, setCategory] = useState<string>('');
-
-  const categories = [
-    { value: 'General Knowledge', label: 'General Knowledge' },
-    { value: 'Entertainment:Books', label: 'Entertainment:Books' },
-    { value: 'Entertainment:Films', label: 'EninitialStatence:Computers' },
-    { value: 'Science:Mathematics', label: 'Science:Mathematics' },
-    { value: 'Mythology', label: 'Mythology' },
-    { value: 'Sports', label: 'Sports' },
-    { value: 'History', label: 'History' },
-    { value: 'Geography', label: 'Geography' },
-    { value: 'Politics', label: 'Politics' },
-    { value: 'Art', label: 'Art' },
-    { value: 'Animal', label: 'Animal' }
-  ];
+  const [category, setCategory] = useState(null);
+  const [level, setLevel] = useState<string>('');
+  const [type, setType] = useState<string>('');
 
   return (
     <div className="app">
       <Nav />
       {page === 'home' && (
         <div className="home-main">
-          <h1>WELCOME TO THE QUIZ APP</h1>
+          <h1 className="home-main__heading">WELCOME TO THE QUIZ APP</h1>
           <div className="home-main__quiz-filter">
             <div className="home-main__filter-item">
               <label>Questions</label>
-              <input type="text" />
+              <input
+                className="home-main__input"
+                onChange={(e: any) => setNumber(e.target.value)}
+                type="number"
+              />
             </div>
             <div className="home-main__filter-item">
               <label>Category</label>
               <Select
+                className="home-main__select"
                 Value={category}
                 options={categories}
-                onChange={(target: any) => setCategory}
+                onChange={() => setCategory}
               />
             </div>
-            <div className="home-main__filter-item"></div>
-            <div className="home-main__filter-item"></div>
-            <div className="home-main__filter-item"></div>
+            <div className="home-main__filter-item">
+              <label>Difficulty</label>
+              <Select
+                className="home-main__select"
+                Value={level}
+                options={difficultyLevel}
+                onChange={(target: any) => setLevel}
+              />
+            </div>
+            <div className="home-main__filter-item">
+              <label>Question Type</label>
+              <Select
+                className="home-main__select"
+                Value={type}
+                options={questionType}
+                onChange={(target: any) => setType}
+              />
+            </div>
           </div>
+          <button className="home-main__btn">Let's Go</button>
         </div>
       )}
       {page === 'quiz' && <div>Hello quiz</div>}
---
+++ src/constants.ts
@@ -0,0 +1,26 @@
+export const categories = [
+  { value: 'General Knowledge', label: 'General Knowledge' },
+  { value: 'Entertainment:Books', label: 'Entertainment:Books' },
+  { value: 'Entertainment:Films', label: 'EninitialStatence:Computers' },
+  { value: 'Science:Mathematics', label: 'Science:Mathematics' },
+  { value: 'Mythology', label: 'Mythology' },
+  { value: 'Sports', label: 'Sports' },
+  { value: 'History', label: 'History' },
+  { value: 'Geography', label: 'Geography' },
+  { value: 'Politics', label: 'Politics' },
+  { value: 'Art', label: 'Art' },
+  { value: 'Animal', label: 'Animal' }
+];
+
+export const difficultyLevel = [
+  { value: 'Any Difficulty', label: 'Any Difficulty' },
+  { value: 'Easy', label: 'Easy' },
+  { value: 'Medium', label: 'Medium' },
+  { value: 'Hard', label: 'Hard' }
+];
+
+export const questionType = [
+  { value: 'Any Type', label: 'Any Type' },
+  { value: 'Multiple Choice', label: 'Multiple Choice' },
+  { value: 'True/False', label: 'True/False' }
+];
