a2bff3a8518b39e23e0ac4d6babc3165578c6722
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-03-12 11:36:03 +0200

    [Feat] Google login

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,32 +1,63 @@
-import React, { useState } from 'react';
-import { GoogleLogin } from 'react-google-login';
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 import logo from '../../assets/quiz.svg';
 import { NavProp } from '../../componentTypes';
 import './nav.scss';
 
-const Nav = ({ setPage }: NavProp) => {
-  const [data, setData] = useState([]);
+type UserData = {
+  email: string;
+  familyName: string;
+  givenName: string;
+  googleId: string;
+  imageUrl: string;
+  name: string;
+};
 
-  const handleLoginSuccess = () => {
-    console.log('success');
+const Nav = ({ setPage }: NavProp) => {
+  const [log, setLog] = useState(false);
+  const [userData, setUserData] = useState<UserData | null>();
+
+  const handleLoginSuccess = (response: any) => {
+    const data = response.profileObj;
+    const stringData = JSON.stringify(data);
+    localStorage.setItem('quiz-user-info', stringData);
+    setLog(true);
   };
+
+  useEffect(() => {
+    const localData = localStorage.getItem('quiz-user-info');
+    const parsedData = localData && JSON.parse(localData);
+    setUserData(parsedData);
+  }, [log]);
 
   const handleLoginFail = () => {
     console.log('failed');
   };
 
+  const handleLogout = () => {
+    setUserData(null);
+    localStorage.removeItem('quiz-user-info');
+    setLog(false);
+  };
+
+  console.log('xxx--', userData);
+
   const googleClient = process.env.REACT_APP_GOOGLE_API as string;
   return (
     <div className="nav">
       <img onClick={() => setPage('home')} className="logo" src={logo} alt="logo" />
-      <GoogleLogin
-        clientId={googleClient}
-        buttonText="Login"
-        onSuccess={handleLoginSuccess}
-        onFailure={handleLoginFail}
-        cookiePolicy={'single_host_origin'}
-      />
+      {!userData ? (
+        <GoogleLogin
+          clientId={googleClient}
+          buttonText="Login"
+          onSuccess={handleLoginSuccess}
+          onFailure={handleLoginFail}
+          cookiePolicy={'single_host_origin'}
+        />
+      ) : (
+        <GoogleLogout clientId={googleClient} buttonText="Log Out" onLogoutSuccess={handleLogout} />
+      )}
     </div>
   );
 };
